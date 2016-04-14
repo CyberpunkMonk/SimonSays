c671b128a49111772b1a1887059edfa0236a4c0c
Connor Boesch <Connor_Boesch@yahoo.com>
2016-04-13 23:16:05 -0500

    Added code to repo.

---
+++ Simon Says/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Simon_Says {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.redButton = new System.Windows.Forms.Button();
+			this.blueButton = new System.Windows.Forms.Button();
+			this.yellowButton = new System.Windows.Forms.Button();
+			this.greenButton = new System.Windows.Forms.Button();
+			this.scoreLabel = new System.Windows.Forms.Label();
+			this.patternLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// redButton
+			// 
+			this.redButton.Location = new System.Drawing.Point(12, 12);
+			this.redButton.Name = "redButton";
+			this.redButton.Size = new System.Drawing.Size(155, 85);
+			this.redButton.TabIndex = 0;
+			this.redButton.Text = "Red";
+			this.redButton.UseVisualStyleBackColor = true;
+			this.redButton.Click += new System.EventHandler(this.redButton_Click);
+			// 
+			// blueButton
+			// 
+			this.blueButton.Location = new System.Drawing.Point(173, 12);
+			this.blueButton.Name = "blueButton";
+			this.blueButton.Size = new System.Drawing.Size(155, 85);
+			this.blueButton.TabIndex = 1;
+			this.blueButton.Text = "Blue";
+			this.blueButton.UseVisualStyleBackColor = true;
+			this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+			// 
+			// yellowButton
+			// 
+			this.yellowButton.Location = new System.Drawing.Point(173, 103);
+			this.yellowButton.Name = "yellowButton";
+			this.yellowButton.Size = new System.Drawing.Size(155, 85);
+			this.yellowButton.TabIndex = 3;
+			this.yellowButton.Text = "Yellow";
+			this.yellowButton.UseVisualStyleBackColor = true;
+			this.yellowButton.Click += new System.EventHandler(this.yellowButton_Click);
+			// 
+			// greenButton
+			// 
+			this.greenButton.Location = new System.Drawing.Point(12, 103);
+			this.greenButton.Name = "greenButton";
+			this.greenButton.Size = new System.Drawing.Size(155, 85);
+			this.greenButton.TabIndex = 2;
+			this.greenButton.Text = "Green";
+			this.greenButton.UseVisualStyleBackColor = true;
+			this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+			// 
+			// scoreLabel
+			// 
+			this.scoreLabel.AutoSize = true;
+			this.scoreLabel.Location = new System.Drawing.Point(12, 191);
+			this.scoreLabel.Name = "scoreLabel";
+			this.scoreLabel.Size = new System.Drawing.Size(68, 20);
+			this.scoreLabel.TabIndex = 4;
+			this.scoreLabel.Text = "Score: 0";
+			// 
+			// patternLabel
+			// 
+			this.patternLabel.AutoSize = true;
+			this.patternLabel.Location = new System.Drawing.Point(173, 191);
+			this.patternLabel.Name = "patternLabel";
+			this.patternLabel.Size = new System.Drawing.Size(129, 20);
+			this.patternLabel.TabIndex = 5;
+			this.patternLabel.Text = "Item in pattern: 0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(343, 241);
+			this.Controls.Add(this.patternLabel);
+			this.Controls.Add(this.scoreLabel);
+			this.Controls.Add(this.yellowButton);
+			this.Controls.Add(this.greenButton);
+			this.Controls.Add(this.blueButton);
+			this.Controls.Add(this.redButton);
+			this.Name = "Form1";
+			this.Text = "Simon Says";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button redButton;
+		private System.Windows.Forms.Button blueButton;
+		private System.Windows.Forms.Button yellowButton;
+		private System.Windows.Forms.Button greenButton;
+		private System.Windows.Forms.Label scoreLabel;
+		private System.Windows.Forms.Label patternLabel;
+	}
+}
+
---
+++ Simon Says/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Simon_Says {
+	public partial class Form1 : Form {
+		int onInList = 0;
+		List<int> pattern = new List<int>();
+		Random rand = new Random();
+		bool playback = false;
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void GUIPlayback() {
+			playback = true;
+			Thread.Sleep(200);
+			foreach (int color in pattern) {
+				switch(color){
+					case 0:
+					redButton.BackColor=Color.Red;
+					Thread.Sleep(250);
+					redButton.BackColor = Color.Transparent;
+					break;
+					case 1:
+					blueButton.BackColor=Color.Blue;
+					Thread.Sleep(250);
+					blueButton.BackColor = Color.Transparent;
+					break;
+					case 2:
+					yellowButton.BackColor=Color.Yellow;
+					Thread.Sleep(250);
+					yellowButton.BackColor = Color.Transparent;
+					break;
+					case 3:
+					greenButton.BackColor=Color.Green;
+					Thread.Sleep(250);
+					greenButton.BackColor = Color.Transparent;
+					break;
+				}
+				Thread.Sleep(200);
+			}
+			playback = false;
+		}
+
+		private void testCorrect(int buttonPressed) {
+			if (playback) return;
+			if (pattern[onInList] == buttonPressed) //BREAKS HERE
+				onInList++;
+			else {
+				MessageBox.Show("Final Score: " + pattern.Count.ToString(),"You failed!");
+				onInList = 0;
+				pattern = new List<int>();
+				new Thread(GUIPlayback).Start();
+			}
+			if (onInList >= pattern.Count) {
+				pattern.Add(rand.Next(0, 4));
+				onInList = 0;
+				new Thread(GUIPlayback).Start();
+			}
+			scoreLabel.Text = "Score: " + pattern.Count;
+			patternLabel.Text = "Item in pattern: " + onInList;
+		}
+
+		private void redButton_Click(object sender, EventArgs e) {
+			testCorrect(0);
+		}
+
+		private void blueButton_Click(object sender, EventArgs e) {
+			testCorrect(1);
+		}
+
+		private void yellowButton_Click(object sender, EventArgs e) {
+			testCorrect(2);
+		}
+
+		private void greenButton_Click(object sender, EventArgs e) {
+			testCorrect(3);
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			pattern.Add(rand.Next(0, 4));
+			new Thread(GUIPlayback).Start();
+		}
+	}
+}
